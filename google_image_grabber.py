28d01fd2664db137ebdf916ac02b7b8729062161
Harsh Bhamore <harsh.bhamore3@gmail.com>
2020-04-02 01:32:13 +0530

    Delete google_image_grabber.py

--- google_image_grabber.py
+++
@@ -1,5 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-arguments = {"keywords":"CSS Tables" , "limit":200 , "print_urls":False , "chromedriver":"/media/bhams/Stuff/table/dataset/table_images_rename/chromedriver"}
-paths = response.download(arguments)
